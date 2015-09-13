5390bce6c7b8f785800f5c6ee383a39ea3be0ef4
Vinicius <vinicius@hadrons.io>
2015-09-13 15:01:32 -0300

    Meta infos from store

--- www/apps/routes.js
+++ www/apps/routes.js
@@ -19,6 +19,16 @@
                                 controller: 'StoresCtrl'
                             }
                         }
+                    })
+                    .state('app.stores.inner', {
+                        url: '/stores/{{stores.id}}',
+                        cache: false,
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'apps/stores/stores-inner.html',
+                                controller: 'StoresInnerCtrl'
+                            }
+                        }
                     })      
                 $urlRouterProvider
                     .otherwise('/app/stores');
--- www/apps/stores/stores-controller.js
+++ www/apps/stores/stores-controller.js
@@ -8,18 +8,7 @@
 					, 'image': 'http://lorempixel.com/400/400/technics/'
 					, activities: ['Celular', 'Som', 'Eletro']
 					, 'store': '216c'
-				},
-				{
-					'name': 'Nokia'
-					, 'image': 'http://lorempixel.com/400/400/sports/'
-					, activities: ['Celular', 'Som', 'Eletro']
-					, 'store': '216c'
-				},
-				{
-					'name': 'Nokia'
-					, 'image': 'http://lorempixel.com/400/400/'
-					, activities: ['Celular', 'Som', 'Eletro']
-					, 'store': '216c'
+					, 'id': '01'
 				}]
         	});
 }());
